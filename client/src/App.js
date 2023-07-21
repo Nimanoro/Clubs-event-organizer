d5454e54303fe31099e79ec345226af48384da46
KoushaAmouzesh <kousha82@@students.cs.ubc.ca>
2023-07-20 17:43:13 -0700

    login page

--- client/src/App.js
+++ client/src/App.js
@@ -4,7 +4,7 @@ import { BrowserRouter as Router, Routes, Route, BrowserRouter } from 'react-rou
 import Home from './pages/Home'
 import Login from './pages/login'
 import Signup from './pages/signup'
-import Header from './components/Header'
+import Header from './components/header'
 
 function App() {
   const [data, setData] = useState('')
